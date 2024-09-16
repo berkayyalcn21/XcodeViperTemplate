27aa1c62cf0218c0d4cdfc217c8eda3de564d350
Ahmet Berkay Yalçın <berkay.yalcin@kariyer.net>
2024-09-16 10:39:13 +0300

    page builder edited

--- Viper.xctemplate/Fullcode/___FILEBASENAME___PageBuilder.swift
+++ Viper.xctemplate/Fullcode/___FILEBASENAME___PageBuilder.swift
@@ -4,7 +4,7 @@ import UIKit
 
 struct ___FILEBASENAMEASIDENTIFIER___ {
     static func make() -> ___VARIABLE_productName:identifier___VC {
-        let destinationVC = ___VARIABLE_productName:identifier___VC()
+        let destinationVC: ___VARIABLE_productName:identifier___VC = UIStoryboard.loadViewController()
         let router = ___VARIABLE_productName:identifier___Router(destinationVC)
         let interactor = ___VARIABLE_productName:identifier___Interactor()
         let presenter = ___VARIABLE_productName:identifier___Presenter(destinationVC, router, interactor)
