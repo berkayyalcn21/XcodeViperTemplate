0f912b0ea7c096e168f9807b1773d45aa991d769
Ahmet Berkay Yalçın <berkay.yalcin@kariyer.net>
2024-09-16 10:48:41 +0300

    vc edited

--- Viper.xctemplate/Fullcode/___FILEBASENAME___VC.swift
+++ Viper.xctemplate/Fullcode/___FILEBASENAME___VC.swift
@@ -2,7 +2,7 @@
 
 import UIKit
 
-final class ___FILEBASENAMEASIDENTIFIER___: BaseVC, ___VARIABLE_productName:identifier___ViewProtocol {
+final class ___FILEBASENAMEASIDENTIFIER___: BaseVC {
     
     var presenter: ___VARIABLE_productName:identifier___PresenterProtocol!
     
@@ -16,7 +16,7 @@ final class ___FILEBASENAMEASIDENTIFIER___: BaseVC, ___VARIABLE_productName:iden
 }
 
 // MARK: - Presenter Outputs
-extension ___FILEBASENAMEASIDENTIFIER___ {
+extension ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_productName:identifier___ViewProtocol {
     func handle(_ output: ___VARIABLE_productName:identifier___PresenterOutputs) {
         
     }
